2bcb99a42bbf9aba83832d568136cf2d54de7ebf
Sartazz7 <sartajislam21@gmail.com>
2021-12-19 23:32:10 +0530

    Django and React

---
+++ leadmanager/leadmanager/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
+    path('', include('leads.urls')),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
+]
